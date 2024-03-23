05428942973d8198e99427f11ead8e368968eb42
jai-riley <145622900+jai-riley@users.noreply.github.com>
2024-03-22 20:49:43 -0600

    Implemented a random_baseline.py

--- SemEval/evaluate.py
+++ SemEval/evaluate.py
@@ -3,43 +3,37 @@ from get_score import get_values
 import os
 import csv
 
-all_dirs = os.listdir("SemEval2024-Task1/output")
-dirs = []
-for file in all_dirs:
-    if file[-8:] == "test.csv":
-        dirs.append(file)
-#dirs.remove("eng_dev.csv")
-# dirs.remove("eng_test.csv")
-#dirs.remove("esp_dev.csv")
-#dirs.remove("kin2eng_test.csv")
-#dirs.remove("kin2eng_dev.csv")
-#dirs.remove("afr2eng_dev.csv")
-#dirs.remove("esp2eng_test.csv")
-dirs = ["track5_eng_eng_test.csv"]
-for file in dirs:
-    print(file)
-    lst = []
-    if file[-8:] == "test.csv" or file[4:8] == "test":
-        label = "SemEval2024-Task1/labels/" + file[0:3] + "_test_with_labels.csv"
-    else:
-        label = "SemEval2024-Task1/labels/" + file[0:3] + "_dev_with_labels.csv"
+all_files = os.listdir("random_baseline")
+file_path = 'random.csv'
+with open(file_path, 'a', newline='') as csvfile:
+    for file in all_files:
+        lst = []
+        print(file[0:7])
+        if file[0:7] == "track4a":
+            label = "SemEval2024-Task1/labels/track4a_sts_esp_eng_test_with_normlabels.csv"
+        elif file[0:7] == "track4b":
+            label = "SemEval2024-Task1/labels/track4b_sts_esp_eng_test_with_normlabels.csv"
+        elif file[-8:] == "test.csv" or file[4:8] == "test":
+            label = "SemEval2024-Task1/labels/" + file[0:3] + "_test_with_labels.csv"
+        elif file[-7:] == "dev.csv" or file[4:7] == "dev":
+            label = "SemEval2024-Task1/labels/" + file[0:3] + "_dev_with_labels.csv"
+        else:
+            label = "SemEval2024-Task1/labels/sts_eng_eng_test_with_normlabels.csv"
 
-    output_file = "SemEval2024-Task1/output/" + file
+        output_file = "random_baseline/" + file
 
-    dev_ys = get_values("sts_eng_eng_test_with_normlabels.csv", "Score")
-    dev_ys_ = get_values(output_file, "Pred_Score")
-    print(len(dev_ys),len(dev_ys_))
-    pearson, y = pearsonr(dev_ys, dev_ys_)
-    spearman, y = spearmanr(dev_ys, dev_ys_)
+        dev_ys = get_values(label, "Score")
+        dev_ys_ = get_values(output_file, "Pred_Score")
+        print(len(dev_ys),len(dev_ys_))
+        pearson, y = pearsonr(dev_ys, dev_ys_)
+        spearman, y = spearmanr(dev_ys, dev_ys_)
 
-    new_row = [file, "THM(0.8)", spearman, pearson]
-    file_path = 'evaluation.csv'
-    # Open the CSV file in append mode
-    with open(file_path, 'a', newline='') as csvfile:
-        # Create a CSV writer object
+        new_row = [file, spearman, pearson]
+        # Open the CSV file in append mode
+            # Create a CSV writer object
         csv_writer = csv.writer(csvfile)
 
-        # Write the new row to the CSV file
+            # Write the new row to the CSV file
         csv_writer.writerow(new_row)
 
 # track 5: 0.70559414430069 0.7397057487960365
@@ -53,14 +47,15 @@ for file in dirs:
 
 # esp: 0.5728845332080186 0.585409019070367, 0.6336848736698238 0.6093109149031243
 # eng: 0.6034921142245867 0.6311834090408295, 0.6443980601117393 0.633632195245917
-"""
+
+
 import csv
 
 # Specify the file path
-file_path = 'evaluation.csv'
+file_path = 'random.csv'
 
 # Specify the column index for which you want to calculate the average
-column_index = 3  # For example, 1 corresponds to the second column (0-indexed)
+column_index = 1  # For example, 1 corresponds to the second column (0-indexed)
 
 # Initialize variables for sum and count
 total = 0
@@ -85,4 +80,5 @@ if count > 0:
 else:
     print("No valid data found in the specified column.")
 
-"""
+
+
---
+++ SemEval/random_baseline.py
@@ -0,0 +1,26 @@
+import csv
+import os
+from random import randint
+
+def get_column(csv_file_path, target_column):
+    with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        if target_column not in csvreader.fieldnames:
+            print(f"Error: Column '{target_column}' not found in the csv file.")
+        else:
+            return [row[target_column] for row in csvreader]
+
+
+all_files = os.listdir("SemEval2024-Task1/labels")
+
+
+print(all_files)
+
+for file in all_files:
+    with open(f"random_baseline/{file}", 'w', newline='', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["PairID","Pred_Score"])
+        ids = get_column(f"SemEval2024-Task1/labels/{file}", "PairID")
+        for pair in ids:
+            num = randint(1, 5)
+            csvwriter.writerow([pair,num/4])
